4a2effad9e404f2abda66a3dec76a0b0b1fe118e
Hans Markwart <hans@rapidrabbit.com>
2024-09-27 14:35:49 +0200

    - added NavigationDestination

--- app/src/main/java/de/hamark/comicreader/ui/Root.kt
+++ app/src/main/java/de/hamark/comicreader/ui/Root.kt
@@ -4,17 +4,25 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import de.hamark.comicreader.ui.NavigationDestination.COMIC_PAGE
 import de.hamark.comicreader.ui.theme.ComicReaderTheme
 
 @Composable
 fun Root() {
     ComicReaderTheme {
         val navController = rememberNavController()
-        NavHost(navController = navController, startDestination = "comicPage") {
-            composable("comicPage") {
+        NavHost(
+            navController = navController,
+            startDestination = COMIC_PAGE.route
+        ) {
+            composable(COMIC_PAGE.route) {
                 PagePane()
             }
         }
     }
+}
+
+enum class NavigationDestination(val route: String) {
+    COMIC_PAGE("comic_page")
 }
 
